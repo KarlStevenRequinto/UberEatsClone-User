ed5424754b1acc4b70c9dd677d3f825ac7b1dd9c
KarlStevenRequinto <lerdrobott@gmail.com>
2023-02-28 23:11:56 +0800

    installing dependencies for aws amplify

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/Navigation";
+import { Amplify, Auth } from "aws-amplify";
+
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 export default function App() {
   return (
@@ -11,4 +16,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
